cd53ad0c768a3f9a5d118e0b371c60d83894dff6
kukeast <88639176+kukeast@users.noreply.github.com>
2022-04-03 23:15:48 +0900

    safari flex

--- src/view/Footer.js
+++ src/view/Footer.js
@@ -5,7 +5,7 @@ import { space } from '../constants/space'
 
 const Wrapper = styled.div`
     display: flex;
-    justify-content: end;
+    justify-content: flex-end;
     width: 100%;
     max-width: ${space.width};
     gap: 16px;
