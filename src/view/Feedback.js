678fd2a6422da57e92509d34d6806df20fa672b2
kukeast <88639176+kukeast@users.noreply.github.com>
2022-04-09 02:09:07 +0900

    auto save

--- src/view/Feedback.js
+++ src/view/Feedback.js
@@ -8,21 +8,38 @@ import Progress from '../components/surface/Progress';
 import { emailkey } from '../util/emailkey';
 import { useNavigate } from 'react-router-dom';
 import { feedbacks } from '../constants/feedbacks';
+import useLocalStorage from '../util/useLocalStorage';
+import Modal from '../components/surface/Modal';
+import Heading2 from '../components/typography/Heading2';
+import Button from '../components/input/Button';
+import Paragraph from '../components/typography/Paragraph';
 
 init(emailkey.USER_ID);
 
 export default function Feedback({ id }) {
     const navigate = useNavigate()
-    const [data, setData] = useState({})
+    const [data, setData] = useLocalStorage([id], {})
     const [currentPage, setCurrentPage] = useState(0)
     const [currentData, setCurrentData] = useState(feedbacks.filter( i => i.id === id)[0].contents[currentPage])
     const [isLoading, setIsLoading] = useState(false)
+    const [timer, setTimer] = useState(10)
+    const [showModal, setShowModal] = useState(false)
 
     useEffect(() => {
         setCurrentData(feedbacks.filter( i => i.id === id)[0].contents[currentPage])
     }, [currentPage, id])
+    useEffect(() => {
+        setShowModal(Object.keys(data)[0] && true)
+        const timerId = setInterval(() => {
+            setTimer(prev => prev + 1)
+        }, 1000)
+        return () => clearInterval(timerId)
+        // eslint-disable-next-line
+    }, [])
 
+    // console.log(data)
     const handleSubmit = () => {
+        window.localStorage.removeItem([id])
         setIsLoading(true)
         const code = GenerateRandomCode.TextNumCode(3,3)
         axios.post(`https://sheet.best/api/sheets/4ba77550-e25c-431b-a213-46ce0788a961/tabs/${id}`, {
@@ -60,18 +77,40 @@ export default function Feedback({ id }) {
     const handlePreviousButton = () => {
         setCurrentPage(prev => prev - 1)
     }
+    const handleCallback = (name, value) => {
+        setData({ 
+            ...data , 
+            [name]: value
+        })
+        setTimer(0)
+    }
+    const handleModalDelete = () => {
+        window.localStorage.removeItem([id])
+        setData({})
+        setShowModal(false)
+    }
     return (
         <Container>
+            {showModal && 
+                <Modal>
+                    <div>
+                        <Heading2>작성 중인 내용이 있어요</Heading2>
+                        <Paragraph>이 전에 작성한 내용을 이어서 쓰시겠어요?</Paragraph>
+                    </div>
+                    <div>
+                        <Button onClick={() => setShowModal(false)}>이어쓰기</Button>
+                        <Button type='danger' onClick={handleModalDelete}>삭제하고 다시쓰기</Button>
+                    </div>
+                </Modal>
+            }
             <Form
                 data={currentData}
                 defaultValue={data[currentData.name]}
-                callback={(name, value) => setData({ 
-                    ...data , 
-                    [name]: value
-                })} 
+                callback={handleCallback} 
                 onClickNext={handleNextButton}
                 onClickPrevious={handlePreviousButton}
                 isLoading={isLoading}
+                autoSave={ timer === 3 ? 'saving' : (timer >= 4 && timer < 5) ? 'done' : 'none'}
             />
             <Progress percent={currentPage/(feedbacks.filter( i => i.id === id)[0].contents.length - 1)*100}/>
         </Container>
