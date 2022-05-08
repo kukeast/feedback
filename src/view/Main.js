d48d20850d7b4f08b1ca2a62ac01860dc58955b4
kukeast <88639176+kukeast@users.noreply.github.com>
2022-05-08 15:29:34 +0900

    피드백 상태 나누기

--- src/view/Main.js
+++ src/view/Main.js
@@ -18,13 +18,28 @@ const Wrapper = styled.div`
     gap: 24px;
 `
 const Label = styled.p`
-    font-size: 16px;
+    display: inline-block;
+    font-size: 14px;
     font-weight: 700;
+    padding: 4px 6px;
+    border-radius: 8px;
+`
+const ScheduledLabel = styled(Label)`
+    color: ${color.yellow[8]};
+    background-color: ${color.yellow[0]};
+`
+const InProgressLabel = styled(Label)`
+    color: ${color.green[8]};
+    background-color: ${color.green[0]};
+`
+const TerminatedLabel = styled(Label)`
     color: ${color.gray[7]};
+    background-color: ${color.gray[1]};
 `
 const Feedbacks = styled.div`
     display: flex;
     flex-direction: column;
+    align-items: flex-start;
     width: 100%;
 `
 const Feedback = styled.div`
@@ -60,25 +75,64 @@ const LinkButton = styled(Link)`
     &:active{
         background-color: ${color.gray[3]};
     }
+    &[disabled]{
+        pointer-events: none;
+        color: ${color.gray[5]};
+        background-color: ${color.gray[0]};
+    }
 `
 
 export default function Main() {
+    const scheduled = feedbacks.filter(feedback => new Date(feedback.startDate) > new Date())
+    const inProgress = feedbacks.filter(feedback => new Date(feedback.startDate) < new Date() && new Date(feedback.endDate) > new Date())
+    const terminated = feedbacks.filter(feedback => new Date(feedback.endDate) < new Date())
+
     return (
         <Container>
             <Wrapper>
                 <Heading1>이동국에게 피드백 보내기</Heading1>
-                <Feedbacks>
-                    <Label>진행 중</Label>
-                    {feedbacks.map(route => (
-                        <Feedback key={route.id}>
-                            <TitleWrapper>
-                                <Title>{route.title}</Title>
-                                <DateRange>{route.startDate}~{route.endDate}</DateRange>
-                            </TitleWrapper>
-                            <LinkButton to={route.path}>작성하기</LinkButton>
-                        </Feedback>
-                    ))}
-                </Feedbacks>
+                {scheduled[0] && 
+                    <Feedbacks>
+                        <ScheduledLabel>예정</ScheduledLabel>
+                        {scheduled.map(route => (
+                            <Feedback key={route.id}>
+                                <TitleWrapper>
+                                    <Title>{route.title}</Title>
+                                    <DateRange>{route.startDate}~{route.endDate}</DateRange>
+                                </TitleWrapper>
+                                <LinkButton to={route.path} disabled>작성하기</LinkButton>
+                            </Feedback>
+                        ))}
+                    </Feedbacks>
+                }
+                {inProgress[0] && 
+                    <Feedbacks>
+                        <InProgressLabel>진행 중</InProgressLabel>
+                        {inProgress.map(route => (
+                            <Feedback key={route.id}>
+                                <TitleWrapper>
+                                    <Title>{route.title}</Title>
+                                    <DateRange>{route.startDate}~{route.endDate}</DateRange>
+                                </TitleWrapper>
+                                <LinkButton to={route.path}>작성하기</LinkButton>
+                            </Feedback>
+                        ))}
+                    </Feedbacks>
+                }
+                {terminated[0] && 
+                    <Feedbacks>
+                        <TerminatedLabel>종료</TerminatedLabel>
+                        {terminated.map(route => (
+                            <Feedback key={route.id}>
+                                <TitleWrapper>
+                                    <Title>{route.title}</Title>
+                                    <DateRange>{route.startDate}~{route.endDate}</DateRange>
+                                </TitleWrapper>
+                                <LinkButton to={route.path} disabled>작성하기</LinkButton>
+                            </Feedback>
+                        ))}
+                    </Feedbacks>
+                }
             </Wrapper>
         </Container>
     )
